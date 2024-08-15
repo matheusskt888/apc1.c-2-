97f64468c6ce5125eca34506b4b7dc219738d1c9
matheusskt888 <160795865+matheusskt888@users.noreply.github.com>
2024-08-15 00:50:17 +0000

    codicos da aulas 1

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,31 @@
+#include<stdio.h>  // inclusao de biblioteca
+
+int main() { //funcao principal
+  // deu codigo aqui
+  // 1. entrada
+  //levantar meus ingredientes
+  // char , int , float , double
+  // variaveis
+  // leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 5;
+  segundo_numero = 6;
+    
+  // 2. processamento
+  // calculo soma, subtracao, multiplica, divesao
+  int soma = primeiro_numero + segundo_numero;
+  
+  
+  
+  // 3. saida
+  // escrita
+  printf(" a soma de %i  com %i eh %i\n", primeiro_numero, 
+ segundo_numero, soma);
+  
+  
+
+  
+
+return 0;  // return fim do programa
+}
